ea0ae20879251aa0abf67787dc72b0b237d34057
Yash Kulkarni <60318826+Selium98@users.noreply.github.com>
2020-06-26 17:00:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body onload="fetch()">
 
 	<nav class="navbar navbar-expand-lg nav_style p-3">
-  <a class="navbar-brand pl-5" href="#">COVID 19</a>
+  <a class="navbar-brand pl-5" href="index.php">COVID 19</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
